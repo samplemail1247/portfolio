fb93e856b72880693069e40d20cbcf96cca4611e
samplemail1247 <84306636+samplemail1247@users.noreply.github.com>
2026-04-14 18:06:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,25 +10,26 @@ const firebaseConfig = {
   apiKey: "AIzaSyCar5tl_EGeRHhvQke8IJITDi_zAArlN8c",
   authDomain: "chat-project-c5409.firebaseapp.com",
   databaseURL: "https://chat-project-c5409-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "chat-project-c5409",
-  storageBucket: "chat-project-c5409.firebasestorage.app",
-  messagingSenderId: "81449624717",
-  appId: "1:81449624717:web:86057269fd96be331aaec4"
+  projectId: "chat-project-c5409"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const chatRef = ref(db, "globalChat");
 
 let username = "";
+let chatRef = null;
 
 // Join
 window.joinChat = function () {
   username = document.getElementById("usernameInput").value.trim();
-  if (!username) return alert("Enter your name!");
+  if (!username) return alert("Enter name!");
+
+  chatRef = ref(db, "chats/" + username);
 
   document.getElementById("login").style.display = "none";
   document.getElementById("chat").style.display = "block";
+
+  listenMessages();
 };
 
 // Send
@@ -46,20 +47,25 @@ window.sendMessage = function () {
   input.value = "";
 };
 
-// Receive
-onChildAdded(chatRef, (snapshot) => {
-  const msg = snapshot.val();
+// Listen
+function listenMessages() {
+  onChildAdded(chatRef, (snapshot) => {
+    const msg = snapshot.val();
 
-  const div = document.createElement("div");
-  div.classList.add("message");
+    const div = document.createElement("div");
+    div.classList.add("message");
 
-  if (msg.user === "ADMIN") {
-    div.classList.add("admin");
-  }
+    // Right side (user)
+    if (msg.user === username) {
+      div.style.textAlign = "right";
+    } else {
+      div.style.color = "#38bdf8"; // admin
+    }
 
-  div.innerText = `${msg.user}: ${msg.text}`;
+    div.innerText = msg.text;
 
-  const messages = document.getElementById("messages");
-  messages.appendChild(div);
-  messages.scrollTop = messages.scrollHeight;
-});
+    const messages = document.getElementById("messages");
+    messages.appendChild(div);
+    messages.scrollTop = messages.scrollHeight;
+  });
+}
